59a9b35f2c13910a0516ba331a6f675d732bcd4a
NrdnKrmt <nourdinkaroumat@gmail.com>
2024-12-08 11:12:55 +0100

    Add Unit Test for getAvailableGamesByUserId Add Integration Test for getAvailableGamesByUserId

--- backend/src/test/java/nrdn/backend/GameControllerTest.java
+++ backend/src/test/java/nrdn/backend/GameControllerTest.java
@@ -5,10 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
+import java.util.Optional;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,21 +26,34 @@ class GameControllerTest {
     @Autowired
     GameRepository gameRepository;
 
+    @MockBean
+    UserRepository userRepository;
+
     @BeforeEach
     void setup() {
         gameRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     @Test
-    void getAllGamesTest() throws Exception {
+    void getAvailableGamesByUserId() throws Exception {
         //GIVEN
-        Game game1 = new Game("4", "League of Legends", "MOBA", "https://picsum.photos/200/300", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam voluptua.");
-        Game game2 = new Game("7", "Throne and Liberty", "MMORPG", "https://picsum.photos/200/300", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam voluptua.");
-        List<Game> games = List.of(game1, game2);
+        Preference preference1 = new Preference("5", "Tank");
+        Preference preference2 = new Preference("6", "Damage Dealer");
+        List<Preference> preferences = List.of(preference1, preference2);
+        User user1 = new User("1", preferences);
+        when(userRepository.findById("1")).thenReturn(Optional.of(user1));
+
+        Game game4 = new Game("4", "League of Legends", "MOBA", "https://picsum.photos/200/300", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam voluptua.");
+        Game game5 = new Game("5", "Guild Wars 2", "MMORPG", "https://picsum.photos/200/300", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam voluptua.");
+        Game game6 = new Game("6", "SMITE", "MOBA", "https://picsum.photos/200/300", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam voluptua.");
+        Game game7 = new Game("7", "Throne and Liberty", "MMORPG", "https://picsum.photos/200/300", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam voluptua.");
+        List<Game> games = List.of(game4, game5, game6, game7);
         gameRepository.saveAll(games);
 
         //WHEN
-        mockMvc.perform(get("/api/games"))
+        mockMvc.perform(get("/api/games/1"))
+
 
         //THEN
                 .andExpect(status().isOk())
--- backend/src/test/java/nrdn/backend/GameServiceTest.java
+++ backend/src/test/java/nrdn/backend/GameServiceTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.when;
 class GameServiceTest {
 
     private final GameRepository mockGameRepo = mock(GameRepository.class);
+    private final UserRepository mockUserRepo = mock(UserRepository.class);
 
     @Test
     void getGameByIdTest_WhenGameExists() {
@@ -22,7 +23,7 @@ class GameServiceTest {
 
         when(mockGameRepo.findById("4")).thenReturn(Optional.of(game1));
 
-        GameService gameService = new GameService(mockGameRepo);
+        GameService gameService = new GameService(mockGameRepo, mockUserRepo);
 
         //WHEN
         Game actual = gameService.getGameById("4");
@@ -36,7 +37,7 @@ class GameServiceTest {
         //GIVEN
         when(mockGameRepo.findById("100")).thenReturn(Optional.empty());
 
-        GameService gameService = new GameService(mockGameRepo);
+        GameService gameService = new GameService(mockGameRepo, mockUserRepo);
 
         //WHEN
 
@@ -45,20 +46,28 @@ class GameServiceTest {
     }
 
     @Test
-    void getAllGamesTest() {
+    void getAvailableGamesByUserIdTest() {
         //GIVEN
-        Game game1 = new Game("4", "League of Legends", "MOBA", "https://picsum.photos/200/300", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam voluptua.");
-        Game game2 = new Game("7", "Throne and Liberty", "MMORPG", "https://picsum.photos/200/300", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam voluptua.");
-        List<Game> games = List.of(game1, game2);
+        Preference preference1 = new Preference("4", "Tank");
+        Preference preference2 = new Preference("7", "Damage Dealer");
+        List<Preference> preferences = List.of(preference1, preference2);
+        User user1 = new User("1", preferences);
+        when(mockUserRepo.findById("1")).thenReturn(Optional.of(user1));
 
+        Game game4 = new Game("4", "League of Legends", "MOBA", "https://picsum.photos/200/300", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam voluptua.");
+        Game game5 = new Game("5", "Guild Wars 2", "MMORPG", "https://picsum.photos/200/300", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam voluptua.");
+        Game game6 = new Game("6", "SMITE", "MOBA", "https://picsum.photos/200/300", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam voluptua.");
+        Game game7 = new Game("7", "Throne and Liberty", "MMORPG", "https://picsum.photos/200/300", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam voluptua.");
+        List<Game> games = List.of(game4, game5, game6, game7);
         when(mockGameRepo.findAll()).thenReturn(games);
 
-        GameService gameService = new GameService(mockGameRepo);
+        GameService gameService = new GameService(mockGameRepo, mockUserRepo);
 
         //WHEN
-        List<Game> actual = gameService.getAllGames();
+        List<Game> actual = gameService.getAvailableGamesByUserId("1");
 
         //THEN
-        assertEquals(games, actual);
+        List<Game> expected = List.of(game5, game6);
+        assertEquals(expected, actual);
     }
 }
