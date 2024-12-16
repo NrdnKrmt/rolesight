a293749efedb66fb343bca09a9fe4273f9d7adad
NrdnKrmt <nourdinkaroumat@gmail.com>
2024-12-16 07:43:42 +0100

    add: oauth login with simple button

---
+++ backend/src/main/java/nrdn/backend/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package nrdn.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(a -> a
+                        .anyRequest().permitAll()                )
+                .sessionManagement(s ->
+                        s.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .oauth2Login(withDefaults());
+
+        return http.build();
+    }
+
+}
--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -3,9 +3,16 @@ import "./Header.css"
 
 function Header() {
 
+    function login() {
+        const host: string = window.location.host === "localhost:5173" ? "http://localhost:8080" : window.location.origin
+
+        window.open(host + "/oauth2/authorization/github", "_self")
+    }
+
     return (
         <div className="header-container">
             <h1><Link to="/">Rolesight</Link></h1>
+            <button onClick={login}>Login</button>
         </div>
     )
 }
