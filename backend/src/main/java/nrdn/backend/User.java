90a7462f7b6c843c01dada0aab9336cf4190f93e
NrdnKrmt <nourdinkaroumat@gmail.com>
2024-12-17 16:53:42 +0100

    feature: user creation

--- backend/src/main/java/nrdn/backend/User.java
+++ backend/src/main/java/nrdn/backend/User.java
@@ -1,9 +1,11 @@
 package nrdn.backend;
 
+import lombok.Builder;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+@Builder
 @Document("users")
 public record User(
         String id,
